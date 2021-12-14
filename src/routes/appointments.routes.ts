f2d199ddad80764bc330b2850121c76c1b1c2c98
Gabriel Cavalheiro <gccavalheiro94@gmail.com>
2021-12-14 00:56:09 -0300

    validando a data de agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,37 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+interface IAppointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments = IAppointment[];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = { id: uuid(), provider, date };
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked.' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
 
   appointments.push(appointment);
 
